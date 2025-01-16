50d7db7fd24bdbc79f01d2489b3a42630895d9e4
DevOfLuv <luvtomeme@proton.me>
2025-01-16 15:50:48 +0100

    update gitignore

--- argruments.js
+++ argruments.js
@@ -14,6 +14,6 @@ module.exports = [
         website: 'luvbomb.meme',
         telegram: 'https://t.me/luvbombmeme',
         twitter: 'https://x.com/luvbombmeme',
-        discord: '-'
+        discord: '-' 
     }
 ]
